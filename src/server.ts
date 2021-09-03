fe4c8024f88a13fc543dd99d502bc2cfb12606f1
Miguel Paiva <miguel.paiva@SPSSJC.IT>
2021-09-03 03:56:48 -0300

    resolução do desafio SOLID

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+const port = 5555;
+
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
